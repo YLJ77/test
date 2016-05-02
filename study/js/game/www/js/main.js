7fadd09d369fd2535d18dcfbc6bf10c94fe3644d
YLJ77 <564296557@qq.com>
2016-05-03 00:34:29 +0800

    game

---
+++ study/js/game/www/js/main.js
@@ -0,0 +1,110 @@
+
+
+function Beat(){
+	var src_bad = ["h0","h1","h2","h3","h4","h5","h6","h7","h8","h9"];
+	var src_kind = ["x0","x1","x2","x3","x4","x5","x6","x7","x8","x9"];
+	var wolf = document.getElementById("wolf");
+	var self = this;
+	this.index = 0;
+	this.src_bad = src_bad;
+	this.src_kind = src_kind;
+	this.src = src_bad;
+	this.wolf = wolf;
+
+	wolf.onclick = function(){
+		clearTimeout(self.timer);
+		self.timer2 = setInterval(function(){self.hited()}, 50)
+	}
+
+	this.clock(this);
+}
+Beat.prototype = {
+	clock : function(obj){
+		var self = this;
+		setTimeout(function(){			
+			obj.timer = setTimeout(arguments.callee,50);
+			self.show(self);
+		},50);
+
+	},
+	show : function(obj){
+		var index = this.index;
+		var wolf = this.wolf;
+		var src_bad = this.src_bad;
+		var src_kind = this.src_kind;
+		var hole = this.hole;
+		var src = this.src;
+		if(index == 0 ) {
+			var num1 = ~~(Math.random() * 9);
+			var hole_sel = hole["hole_"+num1];
+			wolf.style.top = hole_sel["top"];
+			wolf.style.left = hole_sel["left"];
+			var num2 = ~~(Math.random() * 2);
+			src = num2 ? src_bad : src_kind;
+			this.src = src;
+		}
+		wolf.style.display = "block";
+		wolf.src = "img/" + src[index] + ".png";
+		if(index > 4) hit = true;
+		if(index == 0) hit = false;
+		hit ? index-- : index++
+		this.index = index;
+	},
+	hited : function(obj){
+		var index = this.index;
+		var src = this.src;
+		var wolf = this.wolf;
+		index++;
+		if(index >= src.length - 1) {
+			clearInterval(this.timer2);
+			this.clock(this);
+		}
+		console.log(index);
+		wolf.src = "img/" + src[index] + ".png";
+		this.index = index;
+
+	},
+	stop : function(obj){
+		clearTimeout(obj.timer);
+	},
+	hole:{
+		hole_0:{
+			top: "115px",
+			left: "95px"
+		},
+		hole_1:{
+			top: "159px",
+			left: "17px"
+		},
+		hole_2:{
+			top: "141px",
+			left: "187px"
+		},
+		hole_3:{
+			top: "191px",
+			left: "102px"
+		},
+		hole_4:{
+			top: "220px",
+			left: "15px"
+		},
+		hole_5:{
+			top: "211px",
+			left: "199px"
+		},
+		hole_6:{
+			top: "274px",
+			left: "119px"
+		},
+		hole_7:{
+			top: "293px",
+			left: "28px"
+		},
+		hole_8:{
+			top: "295px",
+			left: "205px"
+		}
+	}
+}
+
+new Beat()
